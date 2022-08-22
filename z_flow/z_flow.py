89756dac4d4cf9f4811a0175a9d8c9516e981197
Nielson Janné <nielsonj@gmail.com>
2022-08-22 10:05:05 +0200

    Refactors z-flow to work with new utility classes.
    The message decode method has been removed and functionality has been replaced by LslEventListener.
    The while loop that pushed raw data over LSL has been removed and functionality has been replaced by LslDataPusher.
    The start_all method has been renamed to thread_launcher and launches the 3 main z-flow functions (Graph, LslEventListener, LslDataPusher) in there own threads of control.

--- z_flow/z_flow.py
+++ z_flow/z_flow.py
@@ -1,21 +1,12 @@
 import argparse
 import logging
-import threading
-import time
+from threading import Event
 
-import numpy as np
-from brainflow.board_shim import (BoardIds, BoardShim, BrainFlowInputParams,
-                                  BrainFlowPresets)
+from brainflow.board_shim import (BoardIds, BoardShim, BrainFlowInputParams)
 from brainflow.exit_codes import *
-from pylsl import (StreamInfo, StreamInlet, StreamOutlet, local_clock,
-                   resolve_byprop)
 
-from z_flow.classifiers import Classifier, ClfError
 from z_flow.render import Graph
-
-
-class DecodeError(Exception):
-    pass
+from z_flow.lsl_utility import LslEventListener, BfLslDataPublisher
 
 
 class ZFlow:
@@ -32,7 +23,7 @@ class ZFlow:
         self.classifiers = {}
 
     def __del__(self):
-        if self.board_shim is not None:
+        if isinstance(self.board_shim, BoardShim):
             self.board_shim.stop_stream()
             self.board_shim.release_session()
 
@@ -75,110 +66,33 @@ class ZFlow:
 
         self._create_board_shim(args.board_id, params, args.streamer_params)
 
-        self.start_all()
+        self.module_launcher()
 
-    def message_decode(self, message: str, event_timestamp: float) -> str:
-        """Parsers, decodes and executes LSL events passed as `message`.
-        Returns a success notification or raises and DecodeError.
-
-        :param message: LSL event encoded as str.
-        :type message: str
-        :param event_timestamp: The original even timestamp
-        :type event_timestamp: float
-        :raises DecodeError: "Unknown classifier instance, please create one"
-        :raises DecodeError: "Unrecognized task when decoding"
-        :return: Returns success message
-        :rtype: str
+    def module_launcher(self) -> None:
+        """Method that starts and execute the three main functions of z-flow, in their own thread of control.
         """
-        message_list = message.split(';')
-        task = message_list.pop(0)
-        name = message_list.pop(0)
-        if task == 'create':
-            model_type = message_list.pop(0)
-            time_range = [int(value) for value in message_list.pop(0).split(',')]
-            filter_freq_cutoff = [float(value) for value in message_list.pop(0).split(',')]
-            method = message_list.pop(0)
-            self.classifiers[name] = Classifier(self.board_shim, model_type, time_range, filter_freq_cutoff, method)
-            return f"Created classifier instance, with name {name}."
-        elif task == 'collect' and name in self.classifiers:
-            label = int(message_list.pop(0))
-            self.classifiers[name].collect_sample(label, event_timestamp)
-            return f"Collected sample with, label: {label}, event timestamp: {event_timestamp}."
-        elif task == 'train' and name in self.classifiers:
-            scores = self.classifiers[name].train()
-            return f"Trained model successfully, with scores: {scores}."
-        elif task == 'predict' and name in self.classifiers:
-            prediction, probabilities = self.classifiers[name].predict(event_timestamp)
-            return f"Prediction: {prediction}, with probabilities: {probabilities}"
-        elif name not in self.classifiers:
-            raise DecodeError("Unknown classifier instance, please create one.")
-        else:
-            raise DecodeError("Unrecognized task when decoding.")
+        stay_alive = Event()
+        stay_alive.set()
 
-    def thread_event(self) -> None:
-        dict_clf = {}
-        # create LSL stream to receive events
-        streams = resolve_byprop("name", "SendMarkersOnClick")
-        inlet = StreamInlet(streams[0])
-        # create LSL stream to send information printed on console
-        info = StreamInfo('SendPrintInfo', 'Markers', 1, 0, 'string', 'Zflow_SendPrintInfo')
-        outlet = StreamOutlet(info)
+        graph_thread = Graph(self.board_shim, thread_name="graph_1", thread_daemon=False)
+        lsl_event_listener = LslEventListener(self.board_shim, stay_alive=stay_alive,
+                                              thread_name="lsl_event_listener_1", thread_daemon=False)
+        lsl_data_pusher = BfLslDataPublisher(self.board_shim, stay_alive=stay_alive,
+                                        thread_name="lsl_data_pusher_1", thread_daemon=False)
 
-        while True:
-            event_sample, event_timestamp = inlet.pull_sample()
-            diff = time.time() - local_clock()
-            event_timestamp = event_timestamp+diff  # get timestamp with Unix Epoch format
-            print("got %s at time %s" % (event_sample[0], event_timestamp))
-            outlet.push_sample(["got %s at time %s" % (event_sample[0], event_timestamp)])
-            try:
-                self.message_decode(event_sample[0], event_timestamp)
-            except (DecodeError, ClfError) as e:
-                print(f"Warning: {e} \nStopping thread, please try again.")
+        graph_thread.start()
+        lsl_event_listener.start()
+        lsl_data_pusher.start()
 
-    def start_all(self) -> None:
-        thread1 = threading.Thread(target=Graph, args=(self.board_shim, self.calib_length,
-                                   self.power_length, self.scale, self.offset, self.head_impact), daemon=True)
-        thread1.start()
-        thread2 = threading.Thread(target=self.thread_event, daemon=True)
-        thread2.start()
-        # asyncio.run(thread_event(board_shim, board_id))
+        # Runs as long as z-flow bashboard is not terminated by user.
+        graph_thread.join()
 
-        print(self.board_shim.get_board_descr(self.board_id))
+        # then shutdown remaining threads gracefully.
+        stay_alive.clear()
+        lsl_event_listener.join()
+        lsl_data_pusher.join()
 
-        data_types = {
-            'eeg': BrainFlowPresets.DEFAULT_PRESET,
-            'gyro': BrainFlowPresets.AUXILIARY_PRESET,
-            'ppg': BrainFlowPresets.ANCILLARY_PRESET,
-        }
-        outlets = {}
-
-        for data_type, preset in data_types.items():
-            n_chan = self.board_shim.get_num_rows(self.board_id, preset)
-            rate = self.board_shim.get_sampling_rate(self.board_id, preset)
-            info_data = StreamInfo(f'z-flow-data-{data_type}', data_type, n_chan, rate, 'float32', 'z-flow-send-data')
-            info_data.desc().append_child_value("manufacturer", "Brainflow")
-            info_data.desc().append_child_value("description", str(
-                BoardShim.get_board_descr(self.board_id, preset)))
-            outlets[data_type] = StreamOutlet(info_data)
-
-        previous_timestamp = {'eeg': 0, 'gyro': 0, 'ppg': 0}
-        diff = time.time() - local_clock()
-
-        while True:
-            for data_type, preset in data_types.items():
-                timestamp_column = self.board_shim.get_timestamp_channel(self.board_id, preset=preset)
-
-                # TODO: Potential cause for a bug here in the following lines
-                # if (loop/thread) latency is longer then 1024/sample_rate we skip frames of data. This should be resolved.
-                data = self.board_shim.get_current_board_data(1024, preset)
-                # slice rows with timestamps bigger then previous_timestamp
-                data = data[:, data[timestamp_column] > previous_timestamp[data_type]]
-
-                # only update timestamp and push if there is something left to push.
-                if data.shape[1] > 0:
-                    previous_timestamp[data_type] = data[timestamp_column, -1]
-                    outlets[data_type].push_chunk(data.T.tolist(), previous_timestamp[data_type]-diff)
-            time.sleep(1)
+        print("Closing down z-flow")
 
     def connect(self, board_id: BoardIds, timeout: int, calib_length: int, power_length: int,
                 scale: float, offset: float, head_impact: float, record) -> None:
@@ -200,7 +114,7 @@ class ZFlow:
 
         self._create_board_shim(board_id, params, streamparams)
 
-        self.start_all()
+        self.module_launcher()
 
     def _create_board_shim(self, board_id: BoardIds, params: BrainFlowInputParams, streamparams: str) -> None:
         self.board_id = BoardIds(board_id)
