2649e8f39b20ffd8522d79823a21dbe4b8f9d821
Nielson Janné <nielsonj@gmail.com>
2022-09-07 15:17:13 +0200

    Removed unused Brainflow parameters

--- z_flow/z_flow.py
+++ z_flow/z_flow.py
@@ -35,33 +35,21 @@ class ZFlow:
         # logging.basicConfig(level=logging.DEBUG)
 
         parser = argparse.ArgumentParser()
-        # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-        parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
-                            default=30)
-        parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
-        parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
-                            default=0)
-        parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
-        parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
-        parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
-        parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
-        parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
-        parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+        # use docs to check which parameters are required for specific board, for muse products, see specifically:
+        # https://brainflow.readthedocs.io/en/stable/SupportedBoards.html#muse
         parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
                             required=False, default=BoardIds.MUSE_S_BOARD)
-        parser.add_argument('--file', type=str, help='file', required=False, default='')
+        parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                            default=30)
+        parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+        parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+        parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
         args = parser.parse_args()
 
         params = BrainFlowInputParams()
-        params.ip_port = args.ip_port
-        params.serial_port = args.serial_port
-        params.mac_address = args.mac_address
-        params.other_info = args.other_info
-        params.serial_number = args.serial_number
-        params.ip_address = args.ip_address
-        params.ip_protocol = args.ip_protocol
         params.timeout = args.timeout
-        params.file = args.file
+        params.mac_address = args.mac_address
+        params.serial_number = args.serial_number
 
         self._create_board_shim(args.board_id, params, args.streamer_params)
 
