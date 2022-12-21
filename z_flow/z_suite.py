4efb983d6b3e7c108be3bb3ec6c2b7500a578f41
Nielson Janné <nielsonj@gmail.com>
2022-12-21 23:44:16 +0100

    Organize imports

--- z_flow/z_suite.py
+++ z_flow/z_suite.py
@@ -4,8 +4,8 @@ import tkinter as tk
 import tkinter.font as tkfont
 import tkinter.ttk as ttk
 
-from z_flow.gui import ToolTip
 from z_flow import ZFlow
+from z_flow.gui import ToolTip
 
 
 class ZSuite():
