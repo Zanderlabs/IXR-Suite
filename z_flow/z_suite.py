c3d64f002b88d5043f6f17e3340f4db24a139815
Nielson Janné <nielsonj@gmail.com>
2022-12-21 23:50:28 +0100

    Refactor element creation into seperate methods

--- z_flow/z_suite.py
+++ z_flow/z_suite.py
@@ -34,6 +34,30 @@ class ZSuite():
         titleLabel["text"] = "Z-Suite Mini"
         titleLabel.place(x=0, y=20, width=400, height=25)
 
+        # create input boxes
+        self.create_boardid_input(root)
+        self.create_timeout_input(root)
+        self.create_reference_input(root)
+        self.create_display_ref_input(root)
+        self.create_calibration_input(root)
+        self.create_power_history_input(root)
+        self.create_scale_ent_input(root)
+        self.create_center_ent_input(root)
+        self.create_headstr_ent_input(root)
+
+        # create connect button
+        connectBtn = tk.Button(root, command=self.connectBtn_command)
+        connectBtn["activebackground"] = "#989898"
+        connectBtn["anchor"] = "w"
+        connectBtn["bg"] = "#2a9d8f"
+        ft = tkfont.Font(family='Helvetica', size=13)
+        connectBtn["font"] = ft
+        connectBtn["fg"] = "#000000"
+        connectBtn["justify"] = "center"
+        connectBtn["text"] = "  Connect"
+        connectBtn.place(x=150, y=500, width=100, height=36)
+
+    def create_boardid_input(self, root):
         self.boardidEnt = tk.Entry(root)
         self.boardidEnt["bg"] = "#e9f5db"
         self.boardidEnt["borderwidth"] = "1px"
@@ -55,6 +79,7 @@ class ZSuite():
         boardidLabel.place(x=40, y=80, width=200, height=25)
         self.CreateToolTip(boardidLabel, text='Muse 2 is 38, Muse S is 39 (default = 39)')
 
+    def create_timeout_input(self, root):
         self.timeoutEnt = tk.Entry(root)
         self.timeoutEnt["bg"] = "#e9f5db"
         self.timeoutEnt["borderwidth"] = "1px"
@@ -75,108 +100,76 @@ class ZSuite():
         timeoutLabel.place(x=40, y=120, width=200, height=25)
         self.CreateToolTip(timeoutLabel, text='Timeout when trying to connect to the board (default = 30)')
 
-        self.logBrainDataEnt = tk.Entry(root)
-        self.logBrainDataEnt["bg"] = "#e9f5db"
-        self.logBrainDataEnt["borderwidth"] = "1px"
+    def create_reference_input(self, root):
+        self.reference_ent = tk.Entry(root)
+        self.reference_ent["bg"] = "#e9f5db"
+        self.reference_ent["borderwidth"] = "1px"
         ft = tkfont.Font(family='Helvetica', size=13)
-        self.logBrainDataEnt["font"] = ft
-        self.logBrainDataEnt["fg"] = "#264653"
-        self.logBrainDataEnt["justify"] = "center"
-        self.logBrainDataEnt.insert(tk.END, '0')
-        self.logBrainDataEnt.place(x=250, y=160, width=70, height=25)
+        self.reference_ent["font"] = ft
+        self.reference_ent["fg"] = "#264653"
+        self.reference_ent["justify"] = "center"
+        self.reference_ent.insert(tk.END, 'mean')
+        self.reference_ent.place(x=250, y=160, width=70, height=25)
 
-        braindataLabel = tk.Label(root)
-        braindataLabel["bg"] = "#264653"
+        reference_label = tk.Label(root)
+        reference_label["bg"] = "#264653"
         ft = tkfont.Font(family='Helvetica', size=12)
-        braindataLabel["font"] = ft
-        braindataLabel["fg"] = "#e9f5db"
-        braindataLabel["justify"] = "center"
-        braindataLabel["text"] = "record brain data"
-        braindataLabel.place(x=40, y=160, width=200, height=25)
-        self.CreateToolTip(braindataLabel, text='Will be stored next to this program (default = 0)')
+        reference_label["font"] = ft
+        reference_label["fg"] = "#e9f5db"
+        reference_label["justify"] = "center"
+        reference_label["text"] = "reference"
+        reference_label.place(x=40, y=160, width=200, height=25)
+        self.CreateToolTip(reference_label,
+                           text="Determines what type of re-reference to use.\n"
+                                " - none: No re-referencing is applied.\n"
+                                " - mean (default): Use the mean of the four frontal and temporal electrodes.\n"
+                                " - ref: Use the reference electrode(s) as a reference.")
 
-        self.logConsoleEnt = tk.Entry(root)
-        self.logConsoleEnt["bg"] = "#e9f5db"
-        self.logConsoleEnt["borderwidth"] = "1px"
+    def create_display_ref_input(self, root):
+        self.display_ref_ent = tk.Entry(root)
+        self.display_ref_ent["bg"] = "#e9f5db"
+        self.display_ref_ent["borderwidth"] = "1px"
         ft = tkfont.Font(family='Helvetica', size=13)
-        self.logConsoleEnt["font"] = ft
-        self.logConsoleEnt["fg"] = "#264653"
-        self.logConsoleEnt["justify"] = "center"
-        self.logConsoleEnt.insert(tk.END, '0')
-        self.logConsoleEnt.place(x=250, y=200, width=70, height=25)
+        self.display_ref_ent["font"] = ft
+        self.display_ref_ent["fg"] = "#264653"
+        self.display_ref_ent["justify"] = "center"
+        self.display_ref_ent.insert(tk.END, '0')
+        self.display_ref_ent.place(x=250, y=200, width=70, height=25)
 
-        logConsoleLabel = tk.Label(root)
-        logConsoleLabel["bg"] = "#264653"
+        display_ref_label = tk.Label(root)
+        display_ref_label["bg"] = "#264653"
         ft = tkfont.Font(family='Helvetica', size=12)
-        logConsoleLabel["font"] = ft
-        logConsoleLabel["fg"] = "#e9f5db"
-        logConsoleLabel["justify"] = "center"
-        logConsoleLabel["text"] = "log console"
-        logConsoleLabel.place(x=40, y=200, width=200, height=25)
-        self.CreateToolTip(logConsoleLabel, text='Will be stored next to this program (default = 0)')
+        display_ref_label["font"] = ft
+        display_ref_label["fg"] = "#e9f5db"
+        display_ref_label["justify"] = "center"
+        display_ref_label["text"] = "display reference"
+        display_ref_label.place(x=40, y=200, width=200, height=25)
+        self.CreateToolTip(display_ref_label,
+                           text="Set '1' to displays signal of the reference electrode(s) on the dashboard. ")
 
-        self.calibrationHistoryEnt = tk.Entry(root)
-        self.calibrationHistoryEnt["bg"] = "#e9f5db"
-        self.calibrationHistoryEnt["borderwidth"] = "1px"
+    def create_headstr_ent_input(self, root):
+        self.headStrengthEnt = tk.Entry(root)
+        self.headStrengthEnt["bg"] = "#e9f5db"
+        self.headStrengthEnt["borderwidth"] = "1px"
         ft = tkfont.Font(family='Helvetica', size=13)
-        self.calibrationHistoryEnt["font"] = ft
-        self.calibrationHistoryEnt["fg"] = "#264653"
-        self.calibrationHistoryEnt["justify"] = "center"
-        self.calibrationHistoryEnt.insert(tk.END, '600')
-        self.calibrationHistoryEnt.place(x=250, y=240, width=70, height=25)
+        self.headStrengthEnt["font"] = ft
+        self.headStrengthEnt["fg"] = "#264653"
+        self.headStrengthEnt["justify"] = "center"
+        self.headStrengthEnt.insert(tk.END, '0.2')
+        self.headStrengthEnt.place(x=250, y=400, width=70, height=25)
 
-        calibrationHistoryLabel = tk.Label(root)
-        calibrationHistoryLabel["bg"] = "#264653"
+        headStrength = tk.Label(root)
+        headStrength["bg"] = "#264653"
         ft = tkfont.Font(family='Helvetica', size=12)
-        calibrationHistoryLabel["font"] = ft
-        calibrationHistoryLabel["fg"] = "#e9f5db"
-        calibrationHistoryLabel["justify"] = "center"
-        calibrationHistoryLabel["text"] = "calibration history length"
-        calibrationHistoryLabel.place(x=40, y=240, width=200, height=25)
-        self.CreateToolTip(calibrationHistoryLabel, text='Duration of the rolling calibration in seconds (default = 600)')
+        headStrength["font"] = ft
+        headStrength["fg"] = "#e9f5db"
+        headStrength["justify"] = "center"
+        headStrength["text"] = "head impact strength"
+        headStrength.place(x=40, y=400, width=200, height=25)
+        self.CreateToolTip(
+            headStrength, text='The amount of impact the head movement can have on the brain power (default = 0.2)')
 
-        self.powerHistoryEnt = tk.Entry(root)
-        self.powerHistoryEnt["bg"] = "#e9f5db"
-        self.powerHistoryEnt["borderwidth"] = "1px"
-        ft = tkfont.Font(family='Helvetica', size=13)
-        self.powerHistoryEnt["font"] = ft
-        self.powerHistoryEnt["fg"] = "#264653"
-        self.powerHistoryEnt["justify"] = "center"
-        self.powerHistoryEnt.insert(tk.END, '10')
-        self.powerHistoryEnt.place(x=250, y=280, width=70, height=25)
-
-        powerHistoryLabel = tk.Label(root)
-        powerHistoryLabel["bg"] = "#264653"
-        ft = tkfont.Font(family='Helvetica', size=12)
-        powerHistoryLabel["font"] = ft
-        powerHistoryLabel["fg"] = "#e9f5db"
-        powerHistoryLabel["justify"] = "center"
-        powerHistoryLabel["text"] = "power history length"
-        powerHistoryLabel.place(x=40, y=280, width=200, height=25)
-        self.CreateToolTip(powerHistoryLabel, text='Duration of the current brain power measurement in s (default = 10)')
-
-        self.scaleEnt = tk.Entry(root)
-        self.scaleEnt["bg"] = "#e9f5db"
-        self.scaleEnt["borderwidth"] = "1px"
-        ft = tkfont.Font(family='Helvetica', size=13)
-        self.scaleEnt["font"] = ft
-        self.scaleEnt["fg"] = "#264653"
-        self.scaleEnt["justify"] = "center"
-        self.scaleEnt.insert(tk.END, '1.0')
-        self.scaleEnt.place(x=250, y=320, width=70, height=25)
-
-        scale = tk.Label(root)
-        scale["bg"] = "#264653"
-        ft = tkfont.Font(family='Helvetica', size=12)
-        scale["font"] = ft
-        scale["fg"] = "#e9f5db"
-        scale["justify"] = "center"
-        scale["text"] = "scale"
-        scale.place(x=40, y=320, width=200, height=25)
-        self.CreateToolTip(scale, text='Adjusts the scale of the BCI.\n'
-                      'SMALLER values make it EASIER to reach maximum and minimum \n'
-                      '(recommended between 0.7 and 1.3, default = 1).')
-
+    def create_center_ent_input(self, root):
         self.centerEnt = tk.Entry(root)
         self.centerEnt["bg"] = "#e9f5db"
         self.centerEnt["borderwidth"] = "1px"
@@ -196,39 +189,75 @@ class ZSuite():
         center["text"] = "center"
         center.place(x=40, y=360, width=200, height=25)
         self.CreateToolTip(center, text='The value around which the brainpower should be centered.\n'
-                      'If at 0.5 then your "normal" brain power is 0.5.\n'
-                      'If at 0.3 then your normal brain power is 0.3 and it is more difficult to reach max focus (default = 0.4).')
+                           'If at 0.5 then your "normal" brain power is 0.5.\n'
+                           'If at 0.3 then your normal brain power is 0.3 and it is more difficult to reach max focus (default = 0.4).')
 
-        self.headStrengthEnt = tk.Entry(root)
-        self.headStrengthEnt["bg"] = "#e9f5db"
-        self.headStrengthEnt["borderwidth"] = "1px"
+    def create_scale_ent_input(self, root):
+        self.scaleEnt = tk.Entry(root)
+        self.scaleEnt["bg"] = "#e9f5db"
+        self.scaleEnt["borderwidth"] = "1px"
         ft = tkfont.Font(family='Helvetica', size=13)
-        self.headStrengthEnt["font"] = ft
-        self.headStrengthEnt["fg"] = "#264653"
-        self.headStrengthEnt["justify"] = "center"
-        self.headStrengthEnt.insert(tk.END, '0.2')
-        self.headStrengthEnt.place(x=250, y=400, width=70, height=25)
+        self.scaleEnt["font"] = ft
+        self.scaleEnt["fg"] = "#264653"
+        self.scaleEnt["justify"] = "center"
+        self.scaleEnt.insert(tk.END, '1.0')
+        self.scaleEnt.place(x=250, y=320, width=70, height=25)
 
-        headStrength = tk.Label(root)
-        headStrength["bg"] = "#264653"
+        scale = tk.Label(root)
+        scale["bg"] = "#264653"
         ft = tkfont.Font(family='Helvetica', size=12)
-        headStrength["font"] = ft
-        headStrength["fg"] = "#e9f5db"
-        headStrength["justify"] = "center"
-        headStrength["text"] = "head impact strength"
-        headStrength.place(x=40, y=400, width=200, height=25)
-        self.CreateToolTip(headStrength, text='The amount of impact the head movement can have on the brain power (default = 0.2)')
+        scale["font"] = ft
+        scale["fg"] = "#e9f5db"
+        scale["justify"] = "center"
+        scale["text"] = "scale"
+        scale.place(x=40, y=320, width=200, height=25)
+        self.CreateToolTip(scale, text='Adjusts the scale of the BCI.\n'
+                           'SMALLER values make it EASIER to reach maximum and minimum \n'
+                           '(recommended between 0.7 and 1.3, default = 1).')
 
-        connectBtn = tk.Button(root, command=self.connectBtn_command)
-        connectBtn["activebackground"] = "#989898"
-        connectBtn["anchor"] = "w"
-        connectBtn["bg"] = "#2a9d8f"
+    def create_power_history_input(self, root):
+        self.powerHistoryEnt = tk.Entry(root)
+        self.powerHistoryEnt["bg"] = "#e9f5db"
+        self.powerHistoryEnt["borderwidth"] = "1px"
         ft = tkfont.Font(family='Helvetica', size=13)
-        connectBtn["font"] = ft
-        connectBtn["fg"] = "#000000"
-        connectBtn["justify"] = "center"
-        connectBtn["text"] = "  Connect"
-        connectBtn.place(x=100, y=500, width=90, height=36)
+        self.powerHistoryEnt["font"] = ft
+        self.powerHistoryEnt["fg"] = "#264653"
+        self.powerHistoryEnt["justify"] = "center"
+        self.powerHistoryEnt.insert(tk.END, '10')
+        self.powerHistoryEnt.place(x=250, y=280, width=70, height=25)
+
+        powerHistoryLabel = tk.Label(root)
+        powerHistoryLabel["bg"] = "#264653"
+        ft = tkfont.Font(family='Helvetica', size=12)
+        powerHistoryLabel["font"] = ft
+        powerHistoryLabel["fg"] = "#e9f5db"
+        powerHistoryLabel["justify"] = "center"
+        powerHistoryLabel["text"] = "power history length"
+        powerHistoryLabel.place(x=40, y=280, width=200, height=25)
+        self.CreateToolTip(powerHistoryLabel,
+                           text='Duration of the current brain power measurement in s (default = 10)')
+
+    def create_calibration_input(self, root):
+        self.calibrationHistoryEnt = tk.Entry(root)
+        self.calibrationHistoryEnt["bg"] = "#e9f5db"
+        self.calibrationHistoryEnt["borderwidth"] = "1px"
+        ft = tkfont.Font(family='Helvetica', size=13)
+        self.calibrationHistoryEnt["font"] = ft
+        self.calibrationHistoryEnt["fg"] = "#264653"
+        self.calibrationHistoryEnt["justify"] = "center"
+        self.calibrationHistoryEnt.insert(tk.END, '600')
+        self.calibrationHistoryEnt.place(x=250, y=240, width=70, height=25)
+
+        calibrationHistoryLabel = tk.Label(root)
+        calibrationHistoryLabel["bg"] = "#264653"
+        ft = tkfont.Font(family='Helvetica', size=12)
+        calibrationHistoryLabel["font"] = ft
+        calibrationHistoryLabel["fg"] = "#e9f5db"
+        calibrationHistoryLabel["justify"] = "center"
+        calibrationHistoryLabel["text"] = "calibration history length"
+        calibrationHistoryLabel.place(x=40, y=240, width=200, height=25)
+        self.CreateToolTip(calibrationHistoryLabel,
+                           text='Duration of the rolling calibration in seconds (default = 600)')
 
     def connectBtn_command(self) -> None:
         arguments = [
